4e313675095e8a2651701559fe45f049e7a3d1bb
StephenSanjaya <stephensanjaya5@gmail.com>
2024-05-12 15:27:50 +0700

    add grpc

--- ms-auth/main.go
+++ ms-auth/main.go
@@ -1,5 +1,17 @@
 package main
 
-func main() {
+import (
+	"ms-auth/cmd"
+	"ms-auth/config"
+	"ms-auth/controller"
+	"ms-auth/repository"
+)
 
+func main() {
+	db := config.GetConnection()
+
+	authRepo := repository.NewAuthRepositoryImpl(db)
+	authCtrler := controller.NewAuthControllerImpl(authRepo)
+
+	cmd.InitGrpc(authCtrler.(*controller.AuthControllerImpl))
 }
