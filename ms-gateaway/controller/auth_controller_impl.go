5defe1d841642f17b52a8226849a07cf3082731f
StephenSanjaya <stephensanjaya5@gmail.com>
2024-05-15 11:52:01 +0700

    pull origin

--- ms-gateaway/controller/auth_controller_impl.go
+++ ms-gateaway/controller/auth_controller_impl.go
@@ -1,20 +1,20 @@
 package controller
 
 import (
-    "ms-gateaway/helper"
-    pb "ms-gateaway/pb/auth"
-    "net/http"
+	"ms-gateaway/helper"
+	pb "ms-gateaway/pb/auth"
+	"net/http"
 
-    "github.com/labstack/echo/v4"
-    "google.golang.org/grpc/status"
+	"github.com/labstack/echo/v4"
+	"google.golang.org/grpc/status"
 )
 
 type AuthControllerImpl struct {
-    authGRPC pb.AuthServiceClient
+	authGRPC pb.AuthServiceClient
 }
 
 func NewAuthController(authGRPC pb.AuthServiceClient) AuthControllerI {
-    return &AuthControllerImpl{authGRPC: authGRPC}
+	return &AuthControllerImpl{authGRPC: authGRPC}
 }
 
 // RegisterAuth godoc
@@ -29,23 +29,23 @@ func NewAuthController(authGRPC pb.AuthServiceClient) AuthControllerI {
 // @Failure 500 {object} helper.HTTPError
 // @Router /register [post]
 func (ac *AuthControllerImpl) RegisterAuth(c echo.Context) error {
-    req := &pb.RegisterRequest{}
-    if err := c.Bind(&req); err != nil {
-        return echo.NewHTTPError(http.StatusBadRequest, "invalid body request: "+err.Error())
-    }
+	req := &pb.RegisterRequest{}
+	if err := c.Bind(&req); err != nil {
+		return echo.NewHTTPError(http.StatusBadRequest, "invalid body request: "+err.Error())
+	}
 
-    hashedPass := helper.HashedPassword(req.Password)
-    req.Password = hashedPass
+	hashedPass := helper.HashedPassword(req.Password)
+	req.Password = hashedPass
 
-    res, err := ac.authGRPC.RegisterAuth(c.Request().Context(), req)
-    if err != nil {
-        return echo.NewHTTPError(int(status.Code(err)), "invalid body request: "+err.Error())
-    }
+	res, err := ac.authGRPC.RegisterAuth(c.Request().Context(), req)
+	if err != nil {
+		return echo.NewHTTPError(int(status.Code(err)), "invalid body request: "+err.Error())
+	}
 
-    return c.JSON(http.StatusCreated, echo.Map{
-        "message": "success register",
-        "user":    res,
-    })
+	return c.JSON(http.StatusCreated, echo.Map{
+		"message": "success register",
+		"user":    res,
+	})
 }
 
 // LoginAuth godoc
@@ -60,28 +60,28 @@ func (ac *AuthControllerImpl) RegisterAuth(c echo.Context) error {
 // @Failure 500 {object} helper.HTTPError
 // @Router /login [post]
 func (ac *AuthControllerImpl) LoginAuth(c echo.Context) error {
-    req := &pb.LoginRequest{}
-    if err := c.Bind(&req); err != nil {
-        return echo.NewHTTPError(http.StatusBadRequest, "invalid body request: "+err.Error())
-    }
+	req := &pb.LoginRequest{}
+	if err := c.Bind(&req); err != nil {
+		return echo.NewHTTPError(http.StatusBadRequest, "invalid body request: "+err.Error())
+	}
 
-    res, err := ac.authGRPC.LoginAuth(c.Request().Context(), req)
-    if err != nil {
-        return echo.NewHTTPError(int(status.Code(err)), "login failed: "+err.Error())
-    }
+	res, err := ac.authGRPC.LoginAuth(c.Request().Context(), req)
+	if err != nil {
+		return echo.NewHTTPError(int(status.Code(err)), "login failed: "+err.Error())
+	}
 
-    isPassword := helper.CompareHashedPassword(req.Password, res.Password)
-    if !isPassword {
-        return echo.NewHTTPError(http.StatusBadRequest, "password not match")
-    }
+	isPassword := helper.CompareHashedPassword(req.Password, res.Password)
+	if !isPassword {
+		return echo.NewHTTPError(http.StatusBadRequest, "password not match")
+	}
 
-    token, err := helper.CreateJWT(res)
-    if err != nil {
-        return echo.NewHTTPError(http.StatusInternalServerError, "failed to create token: "+err.Error())
-    }
+	token, err := helper.CreateJWT(res)
+	if err != nil {
+		return echo.NewHTTPError(http.StatusInternalServerError, "failed to create token: "+err.Error())
+	}
 
-    return c.JSON(http.StatusOK, echo.Map{
-        "message": "success login",
-        "jwt":     token,
-    })
+	return c.JSON(http.StatusOK, echo.Map{
+		"message": "success login",
+		"jwt":     token,
+	})
 }
