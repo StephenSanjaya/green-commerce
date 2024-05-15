999a8ef9123e4fb12079268eb67a1e953f961956
StephenSanjaya <stephensanjaya5@gmail.com>
2024-05-16 00:34:24 +0700

    add unimplemented controller

--- ms-order/controller/order_controller_impl.go
+++ ms-order/controller/order_controller_impl.go
@@ -2,7 +2,6 @@ package controller
 
 import (
 	"context"
-	"ms-order/pb/order"
 	pb "ms-order/pb/order"
 	"ms-order/repository"
 
@@ -18,10 +17,20 @@ func NewOrderControllerImpl(repo repository.OrderRepositoryI) OrderControllerI {
 	return &OrderControllerImpl{repo: repo}
 }
 
-func (*OrderControllerImpl) CheckoutOrder(context.Context, *order.CheckoutOrderRequest) (*order.CheckoutOrderResponse, error) {
-	panic("unimplemented")
+func (oc *OrderControllerImpl) CheckoutOrder(c context.Context, pr *pb.CheckoutOrderRequest) (*pb.CheckoutOrderResponse, error) {
+	res, err := oc.repo.CreateOrder(pr)
+	if err != nil {
+		return &pb.CheckoutOrderResponse{}, err
+	}
+
+	return res, nil
 }
 
-func (*OrderControllerImpl) PayOrder(context.Context, *order.PayOrderRequest) (*emptypb.Empty, error) {
-	panic("unimplemented")
+func (oc *OrderControllerImpl) PayOrder(c context.Context, pr *pb.PayOrderRequest) (*emptypb.Empty, error) {
+	_, err := oc.repo.PayOrder(pr)
+	if err != nil {
+		return &emptypb.Empty{}, err
+	}
+
+	return &emptypb.Empty{}, nil
 }
